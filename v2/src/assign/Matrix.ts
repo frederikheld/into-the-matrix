3577d8bb98dc57d02f5b5fcb82fab92855ccbb01
Frederik S. Held <kontakt@frederikheld.de>
2025-02-18 22:14:23 +0100

    Add stats

--- v2/src/assign/Matrix.ts
+++ v2/src/assign/Matrix.ts
@@ -1,28 +1,81 @@
 import { createSymbol, type Symbol } from './Symbol'
 
-export const createMatrix = (parentEl: HTMLDivElement, length: number) => {
+export interface MatrixOptions {
+    maxFps?: number
+}
+
+export interface RenderStats {
+    /**
+     * Number of symbols in the matrix.
+     */
+    symbolCount: number
+    /**
+     * The configured max fps.
+     */
+    maxFps: number
+    /**
+     * The min render duration per frame that is
+     * required to reach the configured max fps.
+     */
+    minFrameTime: number
+    /**
+     * The acutal fps.
+     */
+    fps: number
+    /**
+     * The actual render duration per frame.
+     */
+    frameTime: number
+}
+
+export const createMatrix = (parentEl: HTMLDivElement, length: number, options: MatrixOptions) => {
     const symbols = new Set<Symbol>()
 
-    let isRunning = false
-
     const el = setup(length, symbols)
-
     parentEl.appendChild(el)
 
-    function render () {
-        symbols.forEach((symbol) => symbol.render())
+    // rendering settings.
+    const maxFps: number = options.maxFps || 60
+    const minFrameTime: number = 1000 / maxFps
+
+    // rendering mechanics:
+    let isRunning: boolean = false
+    let previousStartTime: number = performance.now()
+    let currentFrameDuration: number = 1 // time since the previously rendered frame
+
+    // statistics:
+    let previousFrameTime: number = 0 // length of the previously rendered frame
+
+    function render (currentTime: number = performance.now()): void {
+        currentFrameDuration = currentTime - previousStartTime
+
+        if (currentFrameDuration >= minFrameTime) {
+            previousFrameTime = currentFrameDuration // for statistics
+            previousStartTime = currentTime // for rendering mechanics
+            symbols.forEach((symbol) => symbol.render())
+        }
 
         if (isRunning) {
             requestAnimationFrame(render)
         }
     }
 
-    function start () {
+    function getStats () : RenderStats {
+        return {
+            symbolCount: symbols.size,
+            maxFps,
+            minFrameTime,
+            fps: previousFrameTime > 0 ? 1000 / previousFrameTime : 0,
+            frameTime: previousFrameTime
+        }
+    }
+
+    function start () : void {
         isRunning = true
         render()
     }
 
-    function stop () {
+    function stop () : void {
         isRunning = false
     }
 
@@ -30,7 +83,8 @@ export const createMatrix = (parentEl: HTMLDivElement, length: number) => {
         el,
         render,
         start,
-        stop
+        stop,
+        getStats
     }
 }
 
--- v2/src/assign/main.ts
+++ v2/src/assign/main.ts
@@ -8,21 +8,21 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <button id="btnStart">Start</button>
     <button id="btnStop">Stop</button>
   </div>
-  <div id="matrix" style="margin-top: 8px; width: 400px;"></div>
+  <div id="matrix" style="margin-top: 8px; width: 2000px;"></div>
 </div>
 `
 
-const matrix = createMatrix(document.querySelector<HTMLDivElement>('#matrix')!, 100)
-// const matrix = new Matrix(
-//   document.querySelector<HTMLDivElement>('#matrix')!, 1000,
-//   { maxFps: 30 }
-// )
+const matrix = createMatrix(
+    document.querySelector<HTMLDivElement>('#matrix')!,
+    5000,
+    { maxFps: 30 }
+)
 
-// const statsEl = document.querySelector<HTMLDivElement>("#stats")!
-// setInterval(() => {
-//   const stats = matrix.getStats()
-//   statsEl.innerHTML = `frame time: ${Math.ceil(stats.frameTime)} / ${Math.ceil(stats.minFrameTime)} ms | fps: ${Math.ceil(stats.fps)} / ${Math.ceil(stats.maxFps)} | nodes: ${stats.symbolCount}`
-// }, 100)
+const statsEl = document.querySelector<HTMLDivElement>("#stats")!
+setInterval(() => {
+  const stats = matrix.getStats()
+  statsEl.innerHTML = `frame time: ${Math.ceil(stats.frameTime)} / ${Math.ceil(stats.minFrameTime)} ms | fps: ${Math.ceil(stats.fps)} / ${Math.ceil(stats.maxFps)} | nodes: ${stats.symbolCount}`
+}, 100)
 
 document.querySelector<HTMLButtonElement>('#btnStart')!.addEventListener('click', () => matrix.start())
 document.querySelector<HTMLButtonElement>('#btnStop')!.addEventListener('click', () => matrix.stop())
